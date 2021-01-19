f354e954244961b9d9c9bb4430b5beffc403ac91
Jeremy Guill <jeremy_guill@hotmail.com>
2021-01-18 18:15:24 -0600

    first commit

---
+++ Week 1/index.js
@@ -0,0 +1,17 @@
+var itemPrice = 30;
+var amountOfMoneyInWallet = 70;
+var numberOfFriends = 200;
+var ageInYears = 37;
+var firstName = 'Jeremy';
+var lastName = 'Guill';
+var middleInitial = 'T';
+
+let afterpurchase = (amountOfMoneyInWallet - itemPrice);
+
+console.log('After purchase: $' + afterpurchase);
+
+console.log('Ive made this many friends every year of my life: ' + numberOfFriends / ageInYears);
+
+console.log('My Name is: ' + firstName, middleInitial, lastName);
+
+
